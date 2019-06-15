a744fa771f87515f5ac68e9bf8fa84eefb241e92
Hugo Osorio <hugo.o0243@gmail.com>
2019-06-14 19:19:11 -0500

    transicion home a login 5 segundos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//opcional
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.transicionALogin();
+  }
+
+  // hace una transición a la página login despues de 5 segundos
+  transicionALogin() {
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 5000);
+  }
 
 }
