bb8236a1e754a05861a8230b78a3b05d1128c0c6
Abelardo Torres <abelardo.torresperez@yahoo.es>
2019-06-16 23:07:55 -0500

    Se crea page supervisor-tasks y controles

---
+++ src/app/pages/supervisor-tasks/supervisor-tasks.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supervisor-tasks',
+  templateUrl: './supervisor-tasks.page.html',
+  styleUrls: ['./supervisor-tasks.page.scss'],
+})
+export class SupervisorTasksPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
